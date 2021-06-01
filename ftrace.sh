d3f573888c656c4fe4317f398df937db5038f375
Coneagoe <coneagoe@gmail.com>
2021-06-01 09:32:22 +0800

    add ftrace.sh

---
+++ ftrace.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: ftrace.sh
+#
+#         USAGE: ./ftrace.sh
+#
+#   DESCRIPTION: 
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Wu Xufeng (), Xufeng.Wu@nokia-sbell.com
+#  ORGANIZATION: 
+#       CREATED: 06/01/2021 09:28:12 AM
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                                  # Treat unset variables as an error
+
+debugfs=/sys/kernel/debug
+echo nop > $debugfs/tracing/current_tracer
+echo 0 > $debugfs/tracing/tracing_on
+echo $$ > $debugfs/tracing/set_ftrace_pid
+echo function_graph > $debugfs/tracing/current_tracer
+#replace test_proc_show by your function name
+echo test_proc_show > $debugfs/tracing/set_graph_function
+echo 1 > $debugfs/tracing/tracing_on
+exec "$@"
+
